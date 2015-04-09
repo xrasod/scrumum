52a23270eb0fb33480c85aacb3d72d26c338334b
safecoderEXTREME <olaparmros@live.se>
2015-04-09 12:30:07 +0200

    Fixat Spara Utkast i grova drag

--- Scrumproject/GUI/MainWindow.xaml.cs
+++ Scrumproject/GUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Scrumproject.Logic;
 
 namespace Scrumproject
 {
@@ -20,9 +21,26 @@ namespace Scrumproject
     /// </summary>
     public partial class MainWindow : Window
     {
+        Report report = new Report();
+        LogicHandler reportHandler = new LogicHandler();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btnCreateDraft_Click(object sender, RoutedEventArgs e)
+        {
+            report.Description = "HEJHEJHEJ";
+            report.Id = 1;
+            report.NumberOfKilometersDriven = 111;
+            report.UserId = 12;
+            report.Status = 1;
+            reportHandler.SaveDraft(report, "DraftReport.xml");
+            btnCreateDraft.Visibility = Visibility.Hidden;
+        }
+
+        
+
     }
 }
--- Scrumproject/Logic/LogicHandler.cs
+++ Scrumproject/Logic/LogicHandler.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Scrum.Data;
+using Scrum.Data.Data;
 
 namespace Scrumproject.Logic
 {
-    class LogicHandler
-    {
+   public class LogicHandler
+    { 
+      ReportRepository<Report> reportRepository = new ReportRepository<Report>();
+
+
+      public void LoadDraft(string sokvag)
+      {
+          reportRepository.Ladda(sokvag);
+      }
+       
+       public void SaveDraft(Report report, string sokvag)
+       {
+           reportRepository.Spara(report, sokvag);
+       }
+
     }
 }
