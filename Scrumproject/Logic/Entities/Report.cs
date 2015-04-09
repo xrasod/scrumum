f5ad8277ad9c922fabf9a115c17c57bcbcc40f66
safecoderEXTREME <olaparmros@live.se>
2015-04-09 10:50:51 +0200

    Har lagt till entiteter och ID för alla, samt några nya repositories

---
+++ Scrumproject/Logic/Entities/Report.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrumproject.Logic
+{
+    public class Report
+    {
+        public string Id { get; set; }
+    }
+}
--- Scrumproject/Logic/Entities/User.cs
+++ Scrumproject/Logic/Entities/User.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Scrumproject.Data
+namespace Scrumproject.Logic.Entities
 {
-    class BossRepository
+    class User
     {
+        public string Id { get; set; }
     }
 }
