1d8aab9868834b53316237be481825ba012af53e
xtronal <jarvinenemil@hotmail.com>
2015-04-13 15:25:59 +0200

    DangerCoder_CyklaBleat

--- Scrumproject/GUI/MainWindow.xaml.cs
+++ Scrumproject/GUI/MainWindow.xaml.cs
@@ -185,6 +185,31 @@ namespace Scrumproject
             LbTraktamente.Content = content.Subsistence;
         }
 
+        private void btnUploadReceipt_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".png";
+            dlg.Filter =
+                "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                tbReceiptFile.Text = filename;
+            }
+        }
+
      
 
       
--- Scrumproject/Logic/EmailHandler.cs
+++ Scrumproject/Logic/EmailHandler.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Scrumproject.Logic
 {
-    internal class YoloHandler
+    internal class EmailHandler
     {
 
 
