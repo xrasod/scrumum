244c74fae6fa2534bf58defcdfed013511cb38e8
safecoderEXTREME <olaparmros@live.se>
2015-04-16 14:26:49 +0200

    GUI

--- Scrumproject/GUI/MainWindow.xaml.cs
+++ Scrumproject/GUI/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Scrum.Data.Data;
-    using Scrumproject.Data;
-    using Scrumproject.Logic;
+using Scrumproject.Data;
+using Scrumproject.Logic;
 using Scrumproject.Logic.Entities;
 using Scrumproject.GUI;
 
