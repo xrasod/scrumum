eec3f2e448037734e08ffd963012d194c3ddea32
xrasod <rasmus.soderstrom@gmail.com>
2015-04-20 10:30:13 +0200

    Branching master

--- Scrumproject/Logic/XmlReader.cs
+++ Scrumproject/Logic/XmlReader.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-namespace Scrumproject.Logic
-{
-    class XmlReader
-    {
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml;
+//namespace Scrumproject.Logic
+//{
+//    class XmlReader
+//    {
 
-        XmlDocument doc = new XmlDocument();
-            doc.Load("Feeds.xml");
-            var nodes = doc.SelectNodes("ListOfFeeds/ListaAvFeeds/Feed");
-            {
-                foreach (XmlElement node in nodes)
-                {
-                    if (node.SelectSingleNode("ValdKategori").InnerText == kategori)
-                    {
-                        listbox.Items.Add(node.SelectSingleNode("FeedName").InnerText);
-                    }
+//        XmlDocument doc = new XmlDocument();
+//            doc.Load("Feeds.xml");
+//            var nodes = doc.SelectNodes("ListOfFeeds/ListaAvFeeds/Feed");
+//            {
+//                foreach (XmlElement node in nodes)
+//                {
+//                    if (node.SelectSingleNode("ValdKategori").InnerText == kategori)
+//                    {
+//                        listbox.Items.Add(node.SelectSingleNode("FeedName").InnerText);
+//                    }
 
-                }
-            }
+//                }
+//            }
 
 
-    }
-}
+//    }
+//}
