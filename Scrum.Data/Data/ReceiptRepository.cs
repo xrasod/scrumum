fdb7b2e0328c5ba341e6bc7316a6e85c986b03bb
erimeh131 <ericmelin01@gmail.com>
2015-04-23 12:03:17 +0200

    Receipt i PDF, på börjad sortering av rapport

---
+++ Scrum.Data/Data/ReceiptRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrum.Data.Data
+{
+    public class ReceiptRepository
+    {
+        public List<Reciept> GetAllReceipts()
+        {
+            using (var context = new scrumEntities())
+            {
+                return context.Reciepts.OrderBy(x => x.TravelReciept).ToList();
+            }
+        }
+        public Reciept GetSingleReciept(int id)
+        {
+            using (var context = new scrumEntities())
+            {
+                return context.Reciepts.FirstOrDefault(x => x.RID.Equals(id));
+            } 
+        }
+
+    }
+}
--- Scrumproject/Logic/ReportHandler.cs
+++ Scrumproject/Logic/ReportHandler.cs
@@ -18,7 +18,7 @@ namespace Scrumproject.Logic
         PDFRepository pdfRep = new PDFRepository();
         TravelRepository travelRep = new TravelRepository();
         CountriesRepository countryRep = new CountriesRepository();
-
+        ReceiptRepository receiptRep = new ReceiptRepository();
 
 
         public List<string> GetReportList()
@@ -87,6 +87,9 @@ namespace Scrumproject.Logic
             var travelinfos = travelRep.GetAllTravels().Where(x => x.RID == report.RID).ToList();
             var countries = countryRep.GetAllCountries();
             var listOftravelinfos = new List<String>();
+            var receiptInfo = receiptRep.GetAllReceipts().Where(x=> x.RID == report.RID).ToList();
+            var listOfReceipts = new List<String>();
+            
             var statusonreport = "";
             
             foreach (var travel in travelinfos)
@@ -96,13 +99,20 @@ namespace Scrumproject.Logic
             }
             var infoOnTravels = string.Join("\n", listOftravelinfos.ToArray());
             
+            foreach(var receipt in receiptInfo)
+            {
+                var savedReceipts = receiptRep.GetSingleReciept(receipt.RID);
+                listOfReceipts.Add("Kvitto: " + savedReceipts.TravelReciept + " Kostnad: " +" Här ska pris hämtas <3 - " );
+            }
+            var infoOnReceipts = string.Join("\n", listOfReceipts.ToArray());
             var pdfReport = "Inskickad av: " + user.FirstName + " " + user.LastName +"\n" +
                             "Status: " + report.Status + "\n" +
                             "Rapport skapad: " + report.ReportDate.Value.ToShortDateString() + "\n" +
                             "Total summa spenderad: " + report.TotalAmount + "\n" +
                             "Antal kilometer körda: " + report.Kilometers + "\n\n" +
                             "Beskrivning av resa" + "\n" + report.Description + "\n\n" +
-                            "Info om resor" + "\n" + infoOnTravels; 
+                            "Info om resor" + "\n" + infoOnTravels + "\n\n" +
+                            "Info om kvitton \n" + infoOnReceipts; 
 
             pdfRep.createPdfandOpen(pdfReport, DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".pdf");
             ;
